159773489cb77ecf69633e784534fd0d44501bf7
芝本将豊 <masashiba04@gmail.com>
2018-11-09 19:18:52 +0900

    Initial Commit

---
+++ Cash Register/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cash Register
+//
+//  Created by 芝本　将豊 on 2018/11/09.
+//  Copyright © 2018 芝本　将豊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
